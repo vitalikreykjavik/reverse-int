00feaf90dad259c7ee65d6c0b745e7a1e460077f
Vitali Serada <gomelska@gmail.com>
2020-03-22 15:58:27 +0300

    feat: add reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  let result = [];
+  let toProceed = n.toString().split('')
+  for (let i = toProceed.length - 1; i >= 0; i--) {
+    result.push(toProceed[i]);
+  }
+  return parseInt(result.join(''));
+};
